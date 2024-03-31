be24fcabc17445f81fd5d5644f9bc1267b560dd9
shanoo7 <shanoo.kumar11@gmail.com>
2024-03-31 17:38:17 +0530

    adding exchange task

---
+++ exchange return policy/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdownBtns = document.querySelectorAll(".dropdown-btn");
+
+  dropdownBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const dropdownContent = this.nextElementSibling;
+      dropdownContent.classList.toggle("show");
+    });
+  });
+
+  document.addEventListener("click", (event) => {
+    dropdownBtns.forEach(function (btn) {
+      let dropdownContent = btn.nextElementSibling;
+      if (!event.target.matches(".dropdown-btn") && !event.target.matches(".dropdown-content")) {
+        dropdownContent.classList.remove("show");
+      }
+    });
+  });
+});
